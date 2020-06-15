968598885044f3240845fed49178549dbbfca66d
OladetounJed <oladetountemitayo@gmail.com>
2020-06-15 13:22:58 +0100

    Mobile Navbar Animated

--- js/index.js
+++ js/index.js
@@ -6,15 +6,36 @@ console.log("Hello World");
 
 
 menu.addEventListener("click", function(e) {
-  if (e.target.className === "menu") {
-    navbar.style.display = "block";
-  } else if (e.target.className === "close") {
-    navbar.style.display = "none";
-  }
+    setTimeout(function() {
+        if (e.target.className === "menu") {
+            navbar.style.display = "block";
+          } else if (e.target.className === "close") {
+            navbar.style.display = "none";
+          }
+    }, 200)
+ 
 });
 
 close.addEventListener("click", function(e) {
-  if (e.target.className === "close") {
+    setTimeout(function() {
+        if (e.target.className === "close") {
+            navbar.animate([
+                {transform: 'translateX(0px)'},
+                {transform: 'translateX(-900px)'}
+
+            ],
+            {
+                duration: 500
+            }, "linear");
+           
+            
+            
+    } 
+}, 200);
+
+setTimeout(function() {
     navbar.style.display = "none";
-  }
+}, 700)
+  
+  
 });
